337c3ce6a66ac7d1010a801d7d485e6301299248
Rubio <rubiodainton@gmail.com>
2024-11-26 14:24:21 -0300

    Atividade endpoints helloworld, bsm e objetivos

---
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,47 @@
+package com.generation.helloworld.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+	
+		@GetMapping
+		public String helloWorld() {
+			return "Hello World!!!";
+		}
+		
+		@GetMapping("/bsm")
+		public List<String> bsm() {
+			return Arrays.asList(
+					"Lista de habilidades BSM:",
+					"Proatividade",
+	                "Orientação ao Futuro",
+	                "Responsabilidade Pessoal",
+	                "Comunicação",
+	                "Mentalidade de Crescimento",
+	                "Trabalho em Equipe",
+	                "Persistência",
+	                "Orientação ao Detalhe"
+			);
+		}
+		
+		@GetMapping("/objetivos")
+		public List<String> objetivos() {
+			return Arrays.asList(
+					"Objetivos desta semana:",
+					"Aprimorar o conhecimento em Spring Boot",
+	                "Praticar a criação de APIs RESTful",
+	                "Reforçar conceitos de banco de dados e SQL",
+	                "Melhorar habilidades em Java"
+						);
+		}
+	 
+	}
+
+
