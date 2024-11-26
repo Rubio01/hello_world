d2b19fcd7f312205dfc37445f784ad80cc6c6574
Rubio <rubiodainton@gmail.com>
2024-11-26 14:28:07 -0300

    Atividade endpoints helloworld, bsm e objetivos

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -10,38 +10,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
-	
-		@GetMapping
-		public String helloWorld() {
-			return "Hello World!!!";
-		}
-		
-		@GetMapping("/bsm")
-		public List<String> bsm() {
-			return Arrays.asList(
-					"Lista de habilidades BSM:",
-					"Proatividade",
-	                "Orientação ao Futuro",
-	                "Responsabilidade Pessoal",
-	                "Comunicação",
-	                "Mentalidade de Crescimento",
-	                "Trabalho em Equipe",
-	                "Persistência",
-	                "Orientação ao Detalhe"
-			);
-		}
-		
-		@GetMapping("/objetivos")
-		public List<String> objetivos() {
-			return Arrays.asList(
-					"Objetivos desta semana:",
-					"Aprimorar o conhecimento em Spring Boot",
-	                "Praticar a criação de APIs RESTful",
-	                "Reforçar conceitos de banco de dados e SQL",
-	                "Melhorar habilidades em Java"
-						);
-		}
-	 
+
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World!!!";
 	}
 
+	@GetMapping("/bsm")
+	public List<String> bsm() {
+		return Arrays.asList(
+				"Lista de habilidades BSM:",
+				"Proatividade",
+				"Orientação ao Futuro",			
+				"Responsabilidade Pessoal",
+				"Comunicação",
+				"Mentalidade de Crescimento",
+				"Trabalho em Equipe",			
+				"Persistência",
+				"Orientação ao Detalhe");
+	}
 
+	@GetMapping("/objetivos")
+	public List<String> objetivos() {
+		return Arrays.asList(
+				"Objetivos desta semana:",
+				"Aprimorar o conhecimento em Spring Boot",
+				"Praticar a criação de APIs RESTful",
+				"Reforçar conceitos de banco de dados e SQL",			
+				"Melhorar habilidades em Java");
+	}
+
+}
